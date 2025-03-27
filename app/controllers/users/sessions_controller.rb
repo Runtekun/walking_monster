a9e9f7765ad787183d8ebb0ef0edee81a069debe
dec457108 <dec457108@DESKTOP-7F1561K>
2025-03-27 23:42:03 +0900

    ログイン/ログアウト＆ユーザー新規登録

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,35 +1,19 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  before_action :configure_sign_in_params, only: [ :create ]
 
    protected
 
    def configure_sign_in_params
-     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+     devise_parameter_sanitizer.permit(:sign_in, keys: [ :attribute ])
    end
 
-    # サインイン後のリダイレクト先を指定する
    def after_sign_in_path_for(resource)
      root_path
    end
 
-    # サインアウト後のリダイレクト先を指定するメソッドを追加する。
+   # サインアウト後のリダイレクト先を指定するメソッドを追加する。
    def after_sign_out_path_for(resource_or_scope)
      root_path
    end
