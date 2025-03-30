9fb3cf1b92b2a4816da22cb2bd278cd8cbd40340
dec457108 <dec457108@DESKTOP-7F1561K>
2025-03-31 03:54:16 +0900

    投稿作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.includes(:user)
+    end
+
+    def new
+        @post= Board.new
+    end
+
+    def create
+        @post = current_user.boards.build(post_params)
+        if @post.save
+          redirect_to posts_path, success: ("%{item}を作成しました", item: post.model_name.human)
+        else
+          flash.now[:danger] = ("%{item}を作成出来ませんでした", item: post.model_name.human)
+          render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
