1dd833c19f5dd9989aaa7a065768b28465620a9d
dec457108 <dec457108@DESKTOP-7F1561K>
2025-04-03 00:15:49 +0900

    画像アップロード機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_01_102356) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_02_092418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,12 +25,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_01_102356) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "body", null: false
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.string "post_image"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,12 +38,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_01_102356) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", default: "", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "posts", "users"
 end
