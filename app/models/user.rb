205e00a4f0b001f75472c4d84f20cf0e6084442f
dec457108 <dec457108@DESKTOP-7F1561K>
2025-05-05 03:53:25 +0900

    google_login_authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
-      user.avatar = auth.info.image
+      user.remote_avatar_url = auth.info.image
     end
   end
 end
